fbe6d6f7b3c21d91be9b5249135c736e66d43cb6
DEVENDRA MESHRAM <120394539+triggerdeva@users.noreply.github.com>
2023-05-23 23:02:55 +0530

    Update PrivateRoute.js

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,1 +1,17 @@
 
+import React from "react";
+import { useHistory } from "react-router-dom";
+import Login from "./Login";
+import Profile from "./Profile";
+const PrivateRoute= ()=>{
+    const history = useHistory();
+    console.log(history);
+    if(!localStorage.getItem('username') || !localStorage.getItem('password')){
+        history.push('/login');
+        return null;
+    }
+    else{
+        return <Profile />;
+    }
+}
+export default PrivateRoute;
